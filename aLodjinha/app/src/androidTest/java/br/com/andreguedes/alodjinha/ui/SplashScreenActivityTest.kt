fc2a036325260f7367462064a42a3d9f68b8e670
andreguedes <andresguedes@live.com>
2019-05-16 03:47:37 -0300

    Adding splash screen tests

---
+++ aLodjinha/app/src/androidTest/java/br/com/andreguedes/alodjinha/ui/SplashScreenActivityTest.kt
@@ -0,0 +1,42 @@
+package br.com.andreguedes.alodjinha.ui
+
+import android.content.Intent
+import android.support.test.espresso.intent.Intents
+import android.support.test.espresso.intent.Intents.intended
+import android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import br.com.andreguedes.alodjinha.ui.main.MainActivity
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class SplashScreenActivityTest {
+
+    @get:Rule
+    var activityTestRule: ActivityTestRule<SplashScreenActivity> =
+        ActivityTestRule(SplashScreenActivity::class.java)
+
+    @Before
+    fun setup() {
+        Intents.init()
+    }
+
+    @After
+    fun clear() {
+        Intents.release()
+    }
+
+    @Test
+    fun verifyMainScreenIsShowingAfterThis() {
+        activityTestRule.launchActivity(Intent())
+
+        intended(hasComponent(MainActivity::class.java.name))
+    }
+
+}
--- aLodjinha/app/src/androidTest/java/br/com/andreguedes/alodjinha/ui/main/MainActivityTest.kt
+++ aLodjinha/app/src/androidTest/java/br/com/andreguedes/alodjinha/ui/main/MainActivityTest.kt
@@ -1,6 +1,5 @@
 package br.com.andreguedes.alodjinha.ui.main
 
-import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.DrawerActions.close
@@ -12,11 +11,8 @@ import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import br.com.andreguedes.alodjinha.R
-import br.com.andreguedes.alodjinha.ui.SplashScreenTimeIdlingResource
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.not
-import org.junit.After
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -28,19 +24,6 @@ class MainActivityTest {
     @get:Rule
     var activityTestRule: ActivityTestRule<MainActivity> =
         ActivityTestRule(MainActivity::class.java)
-
-    private var idlingResource: SplashScreenTimeIdlingResource? = null
-
-    @Before
-    fun registerIntentServiceIdlingResource() {
-        idlingResource = SplashScreenTimeIdlingResource(1000)
-        Espresso.registerIdlingResources(idlingResource)
-    }
-
-    @After
-    fun unregisterIntentServiceIdlingResource() {
-        Espresso.unregisterIdlingResources(idlingResource)
-    }
 
     @Test
     fun verifyViews() {
